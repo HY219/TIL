a964362c0026a8894c8ff0211caca28eb7e6ccf3
Hyeyeon Jeong <jhy_0219@daum.net>
2023-12-01 14:38:13 +0900

    Revert "What I learned "Week3 ~ Week10 JS, CSS, SCSS, TypeScript, Vue" at FE_DevCourse"

--- FE_DevCourse/Week10/Day48/VUE-START/src/main.js
+++
@@ -1,8 +0,0 @@
-import * as Vue from "vue";
-import App from "./App.vue";
-
-Vue.createApp(App).mount("#app");
-
-// const msg = "Hello Vue?!"
-// const appEl = document.querySelector("#app");
-// appE1.innerHTML = `<h1>${msg}</h1>`
--- FE_DevCourse/Week8/Day38/README.md
+++
@@ -1,7 +0,0 @@
-package.js 생성
-
-```npm init -y```
-
-개발 의존성 package로 typescript 설치
-
-```npm i -D typescript```
