bdc5c748102b08841f42bd2ea473b5a19bde4f9e
hy219 <jhy_0219@daum.net>
2023-12-12 18:27:01 +0900

    Feat: useEffect로 변화 감지 및 전역 이벤트 설정
    * useEffect(변화 감지 후 실행할 동작, 감지 할 것);
    
    * 전역 이벤트 설정 시, 반드시 해제해야한다.
    * return으로 반환한 함수는 컴포넌트가 제거 될 때 실행이 된다.

--- FE_DevCourse/Week12/Day55/kdt-app/src/App.js
+++ FE_DevCourse/Week12/Day55/kdt-app/src/App.js
@@ -1,45 +1,42 @@
-// 요구사항
-// 1. Counter 컴포넌트 구현하기
-// 2. 모든 Counter 컴포넌트의 합 구하기
-
-// 무엇을 배울 것인가
-// 1. 컴포넌트에서 지역 상태 관리하는 법
-// 2. 컴포넌트에 이벤트 바인딩하기
-// 3. 부모 컴포넌트에게 메시지 전달하기
-import { useState } from "react";
-import Counter from "./components/Counter";
+// useEffect
+// 무언가 변화가 있을 때, 감지하여 반응하는 Hook
+import { useEffect, useState } from "react";
 
 function App() {
-  const [totalCount, setTotalCount] = useState(0);
+  const [count, setCount] = useState(0);
+
+  /*
+  useEffect(감지 후 실행할 동작, 감지 할 것(리스트, 배열 o));
+  */
+
+  /* count의 변화를 감지한다. */
+  useEffect(() => {
+    console.log(`Clicked ${count} times.`);
+  }, [count]);
+
+  /* 컴포넌트가 처음 로드될 때 실행된다. */
+  useEffect(() => {
+    console.log(
+      "Component Loaded. 라이프사이클처럼 사용o. 새로고침할 때 감지. - 전역적인(초기) 이벤트를 사용할 때 활용o"
+    );
+    const handleScroll = () => {
+      console.log(window.scrollY);
+    };
+
+    // 전역 이벤트 설정 시
+    document.addEventListener("scroll", handleScroll);
+    // 반드시 해제
+    // return으로 반환한 함수는
+    // 컴포넌트가 제거 될 때 실행이 된다.
+    return () => document.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div>
-      TotalCount: {totalCount}
-      <Counter
-        onIncrease={(count) => {
-          setTotalCount(totalCount + 1);
-        }}
-        onDecrease={(count) => {
-          setTotalCount(totalCount - 1);
-        }}
-      />
-      <Counter
-        onIncrease={(count) => {
-          setTotalCount(totalCount + 1);
-        }}
-        onDecrease={(count) => {
-          setTotalCount(totalCount - 1);
-        }}
-      />
-      <Counter
-        onIncrease={(count) => {
-          setTotalCount(totalCount + 1);
-        }}
-        onDecrease={(count) => {
-          setTotalCount(totalCount - 1);
-        }}
-      />
+      <div>You clicked {count} times.</div>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <div style={{ height: 1000 }}></div>
     </div>
   );
 }
-
 export default App;
