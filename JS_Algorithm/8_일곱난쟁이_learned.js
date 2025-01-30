4bbe4051dbc3197b9606efcb1743a51d52ce1e7c
HY219 <jhy_0219@daum.net>
2025-01-30 23:56:42 +0900

    fix: 일곱난쟁이 풀이 오류 수정

--- JS_Algorithm/8_일곱난쟁이_learned.js
+++ JS_Algorithm/8_일곱난쟁이_learned.js
@@ -1,14 +1,18 @@
 function solution(arr) {
   let answer = arr;
   let sum = arr.reduce((a, b) => a + b, 0); // 누적 값
+  let flag = 0; // 가능한 정답이 여러 가지인 경우 // 2개를 초과하는 인덱스가 제거되지 않도록 변수 이용
 
   for (let i = 0; i < 8; i++) {
     for (let j = i + 1; j < 9; j++) {
       if (sum - (arr[i] + arr[j]) === 100) {
         arr.splice(j, 1); // 뒤에 위치하는 인덱스를 먼저 제거하면,
-        arr.splice(i, 1); // 이전에 위치하는 인뎃그를 제거하는 작업에는 영향을 미치지 않음.
+        arr.splice(i, 1); // 이전에 위치하는 인덱스스를 제거하는 작업에는 영향을 미치지 않음.
+        flag = 1;
+        break;
       }
     }
+    if (flag == 1) break;
   }
 
   return answer;
