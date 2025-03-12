2305b50eb3314f79efff0cb13383ee9bd26aff77
HY219 <jhy_0219@daum.net>
2025-03-13 00:27:49 +0900

    fix: 재귀함수와 스택프레임 학습내용 보충

--- JS_Algorithm/18_재귀함수와스택프레임_learned_2.js
+++ JS_Algorithm/18_재귀함수와스택프레임_learned_2.js
@@ -2,13 +2,13 @@ function solution(n) {
   function DFS(L) {
     if (L == 0) return;
     else {
-      // DFS(L - 1);
-      console.log(L);
       DFS(L - 1);
+      console.log(L);
+      // DFS(L - 1);
     }
   }
 
   DFS(n);
 }
 
-solution(3); // 3 2 1
+solution(3); // 1 2 3
