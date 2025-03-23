bbc45143bfe622cd83e73453482b0c7e0623d249
HY219 <jhy_0219@daum.net>
2025-03-23 16:53:11 +0900

    add: 이진트리순회(DFS) 학습 풀이 추가
    이진트리의 전위, 중위, 후위 순회와 스택프레임

---
+++ JS_Algorithm/20_이진트리순회(DFS).js
@@ -0,0 +1,17 @@
+function solution(v) {
+  function DFS(v) {
+    if (v > 7) {
+      return;
+    } else {
+      console.log(v); // 1 2 4 5 3 6 7 //전위순회
+      DFS(v * 2);
+      /* console.log(v); // 4 2 5 1 6 3 7 // 중위순회 */
+      DFS(v * 2 + 1);
+      /* console.log(v); // 4 5 2 6 7 3 1 // 후위순회 */
+    }
+  }
+
+  DFS(v);
+}
+
+console.log(solution(1));
