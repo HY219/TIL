7212e0f9956ba1f9447e722d77e523acf23a0db4
HY219 <jhy_0219@daum.net>
2025-05-13 23:49:35 +0900

    add: 자릿수의 합 문제를 reduce를 이용한 풀이 추가

---
+++ JS_Algorithm/35_자릿수의합_learned_2.js
@@ -0,0 +1,34 @@
+function solution(input) {
+  const lines = input.trim().split("\n");
+  const N = Number(lines[0]);
+  const arr = lines[1].split(" ").map(Number);
+
+  let answer,
+    maxNum = Number.MIN_SAFE_INTEGER;
+
+  for (let x of arr) {
+    let sum = x
+      .toString()
+      .split("")
+      .reduce((a, b) => a + Number(b), 0);
+
+    if (sum > maxNum) {
+      maxNum = sum;
+      answer = x;
+    } else if (sum === maxNum) {
+      if (x > answer) answer = x;
+    }
+    // 아래의 경우는 answer의 초기값이 undefined이기 때문에 올바른 비교가 안되어서 문제 발생.
+    // if (maxNum <= sum && answer < x) {
+    //   maxNum = sum;
+    //   answer = x;
+    // }
+  }
+
+  return answer;
+}
+
+const input = `7
+128 460 603 40 521 137 123`;
+
+console.log(solution(input));
